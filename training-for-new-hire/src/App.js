9be766980e0e8b64cb203ff0ee0db2f705ee57c5
Prakash Bahadur Singh <prakashbahadursingh2@gmail.com>
2021-06-20 03:28:40 -0700

    initial commit plus initial header content ideas try

---
+++ training-for-new-hire/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          className="headerContent"
+          src="https://www.eastportlandchamberofcommerce.com/resources/Pictures/Adventist%20Health%20new%20logo.png"
+        ></img>
+        <h4 className="lmh">Lodi Memorial Hospital</h4>
+        <h6 className="ltm">Laboratory Training Materials</h6>
+        <div className="LodiLogo App-logo">
+          <i class="fas fa-globe-americas"></i>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
